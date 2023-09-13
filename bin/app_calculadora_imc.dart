7dfe7107c9b9bf323c911a7488504582b1eac028
Mestre Dos Lances <appmestredoslances@gmail.com>
2023-09-13 16:06:15 -0300

    iniciando projeto

---
+++ bin/app_calculadora_imc.dart
@@ -0,0 +1,5 @@
+import 'package:app_calculadora_imc/app_calculadora_imc.dart' as app_calculadora_imc;
+
+void main(List<String> arguments) {
+  print('Hello world: ${app_calculadora_imc.calculate()}!');
+}
---
+++ test/app_calculadora_imc_test.dart
@@ -0,0 +1,8 @@
+import 'package:app_calculadora_imc/app_calculadora_imc.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
